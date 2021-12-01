ac09d7171a529d624cb1f6104aba8830769c4dbb
Alfred Shaker <ashaker@kent.edu>
2021-12-01 08:49:34 -0500

    particles, scriptable objects, multitarget camera

---
+++ IntroGP/Assets/Scripts/Player/PlayerMovementSideScroller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementSideScroller : MonoBehaviour
+{
+
+    Rigidbody rb;
+    public float buttonTime = 0.3f;
+    public float jumpAmount = 5;
+    float jumpTime;
+    bool jumping;
+
+    public float movespeed = 10;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumping = true;
+            jumpTime = 0;
+        }
+
+        if (jumping)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpAmount);
+            jumpTime += Time.deltaTime;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space) | jumpTime > buttonTime)
+        {
+            jumping = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        transform.Translate(new Vector3(h, 0, 0) * movespeed * Time.deltaTime);
+    }
+}
---
+++ IntroGP/Assets/Scripts/ScriptableObjects/CardDisplay.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour {
+
+	public Card card;
+	private Card card2;
+	public Text nameText;
+	public Text descriptionText;
+
+	public Image artworkImage;
+
+	public Text manaText;
+	public Text attackText;
+	public Text healthText;
+
+	// Use this for initialization
+	void Start () {
+		card2 = Resources.Load("ScriptableObjects/Cards/Tirion") as Card;
+		nameText.text = card.name;
+		descriptionText.text = card.description;
+
+		artworkImage.sprite = card.artwork;
+
+		manaText.text = card.manaCost.ToString();
+		attackText.text = card.attack.ToString();
+		healthText.text = card.health.ToString();
+	}
+	
+}
